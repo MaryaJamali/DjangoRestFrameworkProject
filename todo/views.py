2e4ae92c63d6df40642b0602be8e09a405e60535
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-02-03 00:43:52 -0800

    Coding function base view in API using a serializer

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.request import Request
+from rest_framework.response import Response
+from .models import Todo
+from .serializers import TodoSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+# Function_base_view in api view
+@api_view(['GET'])
+def all_todos(request: Request):
+    todos = Todo.objects.order_by('priority').all()
+    todo_serializer = TodoSerializer(todos, many=True)
+    return Response(todo_serializer.data, status.HTTP_202_ACCEPTED)
