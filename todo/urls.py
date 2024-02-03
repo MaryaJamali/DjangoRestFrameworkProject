199bc93094ee49c6b51f16e7e1f908a42115d43b
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-02-03 01:10:36 -0800

    Addressing settings

--- todo/urls.py
+++ todo/urls.py
@@ -13,13 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
-# Addressing App
 urlpatterns = [
-    path('', include('base.urls')),
-    path('todos/', include('todo.urls')),
-    path('api-auth/', include('rest_framework.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.all_todos, name='all_todos_json_api_view'),
 ]
