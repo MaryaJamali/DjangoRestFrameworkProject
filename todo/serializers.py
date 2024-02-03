9eaaa9942637751ca1e5e6cb8eace311c7d10bad
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-02-03 00:31:50 -0800

    Add serializer description

--- todo/serializers.py
+++ todo/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Todo
 
 
+# Converting data in the database to a format such as JSON for use in other applications such as mobile phones
+# is called serializer and vice versa is called de-serializer.
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
