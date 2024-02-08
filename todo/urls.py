9d5e6e2d511f2f05732acf1d6d094d76a844b855
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-02-08 01:12:07 -0800

    Add name in addressing

--- todo/urls.py
+++ todo/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('cbv/', views.TodosListApiView.as_view(), name='todos_list_api_view'),
     path('cbv/<int:todo_id>', views.TodosDetailApiView.as_view(), name='todos_detail_api_view'),
     # Addressing as a class for mixins
-    path('mixins/', views.TodosListMixinApiView.as_view()),
-    path('mixins/<pk>', views.TodosDetailMixinApiView.as_view()),
+    path('mixins/', views.TodosListMixinApiView.as_view(), name='todos_list_mixin_api_view'),
+    path('mixins/<pk>', views.TodosDetailMixinApiView.as_view(), name='todos_detail_mixin_api_view'),
 ]
