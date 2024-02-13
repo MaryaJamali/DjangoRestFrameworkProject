ab4caeb5a6c28a09cccc9e6b33e4b76817056ab4
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-02-13 22:41:17 +0330

    Add description

--- todo/serializers.py
+++ todo/serializers.py
@@ -16,6 +16,7 @@ class TodoSerializer(serializers.ModelSerializer):
 # fetch the user list and in addition to serializing the user information,
 # it displays the serializing todos list of each user.
 class UserSerialzier(serializers.ModelSerializer):
+    # The name of the 'todos' is taken from the 'user' field model of 'related_name' part
     todos = TodoSerializer(read_only=True, many=True)
 
     class Meta:
