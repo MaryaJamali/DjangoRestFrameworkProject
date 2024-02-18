aa2643f049f6a31b7bc668e1c8dba8a41fadfbfc
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-02-18 13:53:31 +0330

    Addresses related to djangorestframework-simplejwt

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView,)
 
 # Addressing App
 urlpatterns = [
@@ -23,5 +24,8 @@ urlpatterns = [
     path('todos/', include('todo.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('auth-token/', obtain_auth_token, name='generate_auth_token'),
+    # Addresses related to djangorestframework-simplejwt
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
 ]
